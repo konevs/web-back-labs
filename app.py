ef649a5a8d61395608e6c96bb5bad6962be66a2d
Стас Конев <konevs2945@mail.com>
2025-10-31 13:36:46 +0300

    лабораторная работа 2

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def index():
         <nav>
             <ul>
                 <li><a href="/lab1">Первая лабораторная</a></li>
+                <li><a href="/lab2">Вторая лабораторная</a></li>
             </ul>
         </nav>
         <footer>
@@ -297,32 +298,159 @@ def a2():
 
 flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
 
+@app.route('/lab2/add_flower/')
+def add_flower_no_name():
+    return 'вы не задали имя цветка', 400
+
+
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+    <!doctype html>
+    <html>
+        <body>
+            <h1>Добавлен новый цветок</h1>
+            <p>Название нового цветка: {name}</p>
+            <p>Всего цветов: {len(flower_list)}</p>
+            <p>Полный список: {flower_list}</p>
+            <p><a href="/lab2/flowers/">Посмотреть все цветы</a></p>
+        </body>
+    </html>
+    '''
+
+@app.route('/lab2/flowers/')
+def all_flowers():
+    flowers_html = '<ul>'
+    for f in flower_list:
+        flowers_html += f'<li>{f}</li>'
+    flowers_html += '</ul>'
+    return f'''
+    <!doctype html>
+    <html>
+        <head><meta charset="utf-8"><title>Все цветы</title></head>
+        <body>
+            <h1>Список всех цветов</h1>
+            {flowers_html}
+            <p>Всего цветов: {len(flower_list)}</p>
+            <p><a href="/lab2/">Назад к лабораторной</a></p>
+        </body>
+    </html>
+    '''
+
+
 @app.route('/lab2/flowers/<int:flower_id>')
 def flowers(flower_id):
     if flower_id >= len(flower_list):
         abort(404)
-    else:    
-        return "цветок: " + flower_list[flower_id]
-@app.route('/lab2/add_flower/<name>')
-
-def add_flower(name):
-    flower_list.append(name)
+    flower = flower_list[flower_id]
     return f'''
 <!doctype html>
-<html>
-    <body>
-    <h1>Добавлен новый цветок</h1>
-    <p>Название нового цветка: {name} </p>
-    <p>Всего цветов: {len(flower_list)}</p> 
-    <p>Полный список: {flower_list}</p>
-    </body> 
-</html>   
-'''      
+    <html>
+        <head><meta charset="utf-8"><title>{flower}</title></head>
+        <body>
+            <h1>Цветок: {flower}</h1>
+            <p>Индекс: {flower_id}</p>
+            <p><a href="/lab2/flowers/">Посмотреть все цветы</a></p>
+        </body>
+    </html>
+    '''
+
+@app.route('/lab2/flowers/clear')
+def clear_flowers():
+    flower_list.clear()
+    return redirect('/lab2/flowers/')
 
 @app.route('/lab2/example')
 def example():
-    name = 'Станислав Конев'
-    number = 'Лабораторная работа 2'
-    group = 'ФБИ-34'
-    course = '3 курс'
-    return render_template('example.html', name=name, number=number, group=group, course=course)
+    name, number, group, course = 'Станислав Конев',  '2', 'ФБИ-34',  '3 курс'
+    fruits = [
+        {'name': 'яблоки', 'price': 100},
+        {'name': 'груши', 'price': 120},
+        {'name': 'апельсины', 'price': 80},
+        {'name': 'мандарины', 'price': 95},
+        {'name': 'манго', 'price': 321}
+    ]
+    return render_template('example.html', name=name, number=number, group=group, 
+                           course=course, fruits=fruits)
+
+@app.route('/lab2/')
+def lab2(): 
+    return render_template('lab2.html')
+
+@app.route('/lab2/filters')
+def filters():
+    phrase = "О <b>сколько</b> <u>нам</u> <i>открытий</i> чудных..."
+    return render_template('filter.html', phrase=phrase)
+
+@app.route('/lab2/calc/<int:a>/<int:b>')
+def calc(a, b):
+    return f'''
+    <!doctype html>
+    <html>
+    <head><meta charset="utf-8"><title>Калькулятор</title></head>
+    <body>
+        <h1>Калькулятор</h1>
+        <ul>
+            <li>{a} + {b} = {a + b}</li>
+            <li>{a} - {b} = {a - b}</li>
+            <li>{a} * {b} = {a * b}</li>
+            <li>{a} / {b if b != 0 else 1} = {a / b if b != 0 else 'Нельзя делить на 0'}</li>
+            <li>{a} ** {b} = {a ** b}</li>
+        </ul>
+        <p><a href="/lab2/">Назад</a></p>
+    </body>
+    </html>
+    '''
+
+@app.route('/lab2/calc/')
+def calc_default():
+    return redirect('/lab2/calc/1/1')
+
+@app.route('/lab2/calc/<int:a>')
+def calc_a(a):
+    return redirect(f'/lab2/calc/{a}/1')
+
+books = [
+    {'author': 'Дж. Роулинг', 'title': 'Гарри Поттер', 'genre': 'Фэнтези', 'pages': 400},
+    {'author': 'Дж. Толкин', 'title': 'Властелин колец', 'genre': 'Фэнтези', 'pages': 800},
+    {'author': 'А. Кристи', 'title': 'Убийство в Восточном экспрессе', 'genre': 'Детектив', 'pages': 250},
+    {'author': 'С. Кинг', 'title': 'Оно', 'genre': 'Ужасы', 'pages': 700},
+    {'author': 'Э. Ремарк', 'title': 'Три товарища', 'genre': 'Роман', 'pages': 350},
+    {'author': 'Г. Уэллс', 'title': 'Война миров', 'genre': 'Фантастика', 'pages': 280},
+    {'author': 'А. Дюма', 'title': 'Три мушкетера', 'genre': 'Приключения', 'pages': 600},
+    {'author': 'Д. Оруэлл', 'title': '1984', 'genre': 'Антиутопия', 'pages': 320},
+    {'author': 'Р. Брэдбери', 'title': '451 по Фаренгейту', 'genre': 'Фантастика', 'pages': 200},
+    {'author': 'Х. Мураками', 'title': 'Норвежский лес', 'genre': 'Роман', 'pages': 380}
+]
+
+@app.route('/lab2/books')
+def books_list():
+    return render_template('books.html', books=books)
+
+fruits = [
+    {"name": "Яблоко", "desc": "Сочное красное яблоко", "img": "apple.jpg"},
+    {"name": "Банан", "desc": "Спелый желтый банан", "img": "banana.jpg"},
+    {"name": "Апельсин", "desc": "Сладкий сочный апельсин", "img": "orange.jpg"},
+    {"name": "Клубника", "desc": "Свежая ароматная клубника", "img": "strawberry.jpg"},
+    {"name": "Виноград", "desc": "Сладкий зеленый виноград", "img": "grape.jpg"},
+    {"name": "Персик", "desc": "Мягкий спелый персик", "img": "peach.jpg"},
+    {"name": "Груша", "desc": "Сочная сладкая груша", "img": "pear.jpg"},
+    {"name": "Ананас", "desc": "Спелый тропический ананас", "img": "pineapple.jpg"},
+    {"name": "Манго", "desc": "Ароматное спелое манго", "img": "mango.jpg"},
+    {"name": "Киви", "desc": "Свежий зеленый киви", "img": "kiwi.jpg"},
+    {"name": "Лимон", "desc": "Ароматный желтый лимон", "img": "lemon.jpg"},
+    {"name": "Гранат", "desc": "Спелый рубиновый гранат", "img": "pomegranate.jpg"},
+    {"name": "Арбуз", "desc": "Сладкий сочный арбуз", "img": "watermelon.jpg"},
+    {"name": "Дыня", "desc": "Ароматная спелая дыня", "img": "melon.jpg"},
+    {"name": "Мандарин", "desc": "Сладкий цитрусовый мандарин", "img": "mandarin.jpg"},
+    {"name": "Слива", "desc": "Сочная фиолетовая слива", "img": "plum.jpg"},
+    {"name": "Вишня", "desc": "Кисло-сладкая красная вишня", "img": "cherry.jpg"},
+    {"name": "Черешня", "desc": "Сладкая темная черешня", "img": "sweet_cherry.jpg"},
+    {"name": "Абрикос", "desc": "Нежный спелый абрикос", "img": "apricot.jpg"},
+    {"name": "Инжир", "desc": "Спелый сладкий инжир", "img": "fig.jpg"}
+]
+
+@app.route('/lab2/fruits')
+def vegetables_gallery():
+    return render_template('fruits.html', fruits=fruits)
